3ff2a8e43c7022ee152caab9bfb3ce042b056cc2
saifeddineelamri <saifamri156@gmail.com>
2024-12-07 13:44:02 +0100

    changement

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
